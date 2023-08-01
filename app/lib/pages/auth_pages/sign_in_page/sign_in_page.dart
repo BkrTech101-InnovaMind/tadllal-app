85c41c4ee6ec702451c2c8fcad690cbd6cc83404
Abobakr Mahdi <93738112+BKR779@users.noreply.github.com>
2023-08-01 22:42:07 +0300

    refactor(sign-in-page): implement a new account button and change the content from being a column to a single scroll to never being layedout

--- app/lib/pages/auth_pages/sign_in_page/sign_in_page.dart
+++ app/lib/pages/auth_pages/sign_in_page/sign_in_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:tadllal/pages/auth_pages/sign_in_page/widgets/sign_in_form.dart';
+import 'package:tadllal/pages/auth_pages/sign_up_page/sign_up_page.dart';
+import 'package:tadllal/pages/auth_pages/widgets/share_button.dart';
 
 class SignInPage extends StatefulWidget {
   const SignInPage({super.key});
@@ -15,12 +17,60 @@ class _SignInPageState extends State<SignInPage> {
       textDirection: TextDirection.rtl,
       child: SafeArea(
         child: Scaffold(
-          body: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              buildSignInIntro(context),
-              const SignInForm(),
-            ],
+          body: SingleChildScrollView(
+            child: Column(
+              children: [
+                buildSignInIntro(context),
+                Container(
+                  margin: const EdgeInsets.symmetric(horizontal: 15),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      const SignInForm(),
+                      const SizedBox(height: 20),
+                      Row(
+                        children: [
+                          const Expanded(child: Divider(color: Colors.black45)),
+                          Container(
+                            padding: const EdgeInsets.symmetric(horizontal: 10),
+                            child: const Text("أو"),
+                          ),
+                          const Expanded(child: Divider(color: Colors.black45)),
+                        ],
+                      ),
+                      const SizedBox(height: 20),
+                      OutlinedButton(
+                        style: OutlinedButton.styleFrom(
+                          foregroundColor: const Color(0xFF1F4C6B),
+                          fixedSize: const Size(278, 63),
+                          shape: const RoundedRectangleBorder(
+                            borderRadius: BorderRadius.all(Radius.circular(10)),
+                          ),
+                          side: const BorderSide(
+                              color: Color(0xFF8BC83F), width: 3),
+                        ),
+                        onPressed: () {
+                          Navigator.push<void>(
+                            context,
+                            MaterialPageRoute<void>(
+                              builder: (BuildContext context) =>
+                                  const SignUpPage(),
+                            ),
+                          );
+                        },
+                        child: const Text(
+                          "إنشاء حساب جديد",
+                          style: TextStyle(
+                              fontSize: 22, fontWeight: FontWeight.bold),
+                        ),
+                      ),
+                      const SizedBox(height: 30),
+                      const ShareButton()
+                    ],
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),

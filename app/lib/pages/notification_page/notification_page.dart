3785b9e3f56a27418ab2712de0647a4f93201b8c
Abobakr Mahdi <zabobaker7355@gmail.com>
2023-08-16 03:33:11 +0300

    feat(notification_page): create a notification page that let the user to know when there is a new realestate been listed

---
+++ app/lib/pages/notification_page/notification_page.dart
@@ -0,0 +1,154 @@
+import 'package:flutter/material.dart';
+import 'package:tadllal/pages/real_estate_details_page/real_estate_details_page.dart';
+
+final List<Map<String, dynamic>> realEstates = [
+  {
+    'images': [
+      'assets/images/shape.png',
+      'assets/images/shape2.png',
+      'assets/images/shape3.png',
+      'assets/images/shape4.png',
+    ],
+    "availability": "متاح",
+    "state": "للإيجار",
+    "location": "حدة,حي العفيفة",
+    "price": "220",
+    "type": "أرض",
+    "title": "شقة مفروشة للإيجار",
+    "description":
+        "شقة مفروشة للايجار في ارقى احياء صنعاء مجهزة باحدث وسائل الراحة الممكنه التي قد تحلم بها في حياتك ",
+    "rating": "4.8",
+    "isFavorite": false
+  },
+  {
+    'images': [
+      'assets/images/shape.png',
+      'assets/images/shape2.png',
+      'assets/images/shape3.png',
+      'assets/images/shape4.png',
+    ],
+    "availability": "غير متاح",
+    "state": "للبيع",
+    "type": "شقة",
+    "location": "بيت بوس, حي الشباب",
+    "price": "500,000",
+    "title": "فيلا بتصميم حديث",
+    "description":
+        "شقة مفروشة للايجار في ارقى احياء صنعاء مجهزة باحدث وسائل الراحة الممكنه التي قد تحلم بها في حياتك ",
+    "rating": "4.8",
+    "isFavorite": false
+  },
+  {
+    'images': [
+      'assets/images/shape.png',
+      'assets/images/shape2.png',
+      'assets/images/shape3.png',
+      'assets/images/shape4.png',
+    ],
+    "availability": "متاح",
+    "state": "للإيجار",
+    "type": "عمارة",
+    "location": "بيت بوس, حي الشباب",
+    "price": "235",
+    "title": "فيلا واسعة للإيجار",
+    "description":
+        "شقة مفروشة للايجار في ارقى احياء صنعاء مجهزة باحدث وسائل الراحة الممكنه التي قد تحلم بها في حياتك ",
+    "rating": "4.8",
+    "isFavorite": false
+  },
+  {
+    'images': [
+      'assets/images/shape.png',
+      'assets/images/shape2.png',
+      'assets/images/shape3.png',
+      'assets/images/shape4.png',
+    ],
+    "availability": "غير متاح",
+    "state": "للبيع",
+    "type": "فلة",
+    "location": "حدة,شارع بيروت",
+    "price": "56,000",
+    "title": "شقة للبيع في برج حديث وفي موقع متميز",
+    "description":
+        "شقة مفروشة للايجار في ارقى احياء صنعاء مجهزة باحدث وسائل الراحة الممكنه التي قد تحلم بها في حياتك ",
+    "rating": "4.8",
+    "isFavorite": false
+  },
+];
+
+class NotificationPage extends StatefulWidget {
+  final bool isHasNotification;
+  const NotificationPage({required this.isHasNotification, super.key});
+
+  @override
+  State<NotificationPage> createState() => _NotificationPageState();
+}
+
+class _NotificationPageState extends State<NotificationPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("الإشعارات"),
+        centerTitle: true,
+        backgroundColor: const Color(0xFF194706),
+      ),
+      body: Container(
+        margin: const EdgeInsets.symmetric(vertical: 20),
+        child: ListView.separated(
+            itemCount: realEstates.length,
+            separatorBuilder: (context, index) => const Divider(thickness: 2),
+            itemBuilder: (context, index) {
+              return Container(
+                margin: const EdgeInsets.only(top: 15),
+                child: ListTile(
+                  onTap: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => RealEstateDetailsPage(
+                          realEstate: realEstates[index],
+                        ),
+                      ),
+                    );
+                  },
+                  title: Text(
+                    realEstates[index]['title'],
+                    maxLines: 1,
+                    overflow: TextOverflow.ellipsis,
+                  ),
+                  subtitle: Text(
+                    realEstates[index]['description'],
+                    maxLines: 1,
+                    overflow: TextOverflow.ellipsis,
+                  ),
+                  leading: CircleAvatar(
+                    backgroundImage:
+                        AssetImage(realEstates[index]['images'][0]),
+                  ),
+                  trailing: Container(
+                    constraints: BoxConstraints.tight(const Size.square(60)),
+                    decoration: BoxDecoration(
+                        color: Colors.grey[300],
+                        borderRadius: BorderRadius.circular(10)),
+                    width: 60,
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        Text(realEstates[index]['rating'].toString()),
+                        const Icon(
+                          Icons.star,
+                          color: Color(0xFFE0A410),
+                          size: 20,
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              );
+            }),
+      ),
+    );
+  }
+}

7aef8a681fb09d218b240f8e4d6f7afc0a463537
Abobakr Mahdi <93738112+BKR779@users.noreply.github.com>
2023-08-29 03:46:25 +0300

    style(dashbord-layout): fix some design issue

--- dashboard/src/layout/Layout.jsx
+++ dashboard/src/layout/Layout.jsx
@@ -1,15 +1,14 @@
-import Nav from '@/Components/Navbar'
-import Sidebar from '@/Components/Sidebar'
-import React from 'react'
+import Nav from "@/Components/Navbar"
+import Sidebar from "@/Components/Sidebar"
 
 export default function Layout({ children }) {
-    return (
-        <div className="flex flex-row-reverse bg-backGraound w-full">
-            <Sidebar />
-            <div className="flex flex-col w-full  p-4 bg-backGraound">
-                <Nav />
-                {children}
-            </div>
-        </div>
-    )
+  return (
+    <div className='flex flex-row bg-backGraound' dir='rtl'>
+      <Sidebar />
+      <div className='flex-1 flex-col w-full p-4 bg-backGraound'>
+        <Nav />
+        {children}
+      </div>
+    </div>
+  )
 }

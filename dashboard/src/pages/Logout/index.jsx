01654ff2d241f19569ced3f981123e2c60b77a3b
Abobakr Mahdi <93738112+BKR779@users.noreply.github.com>
2023-08-30 09:38:29 +0300

    docs(tadllal-dashboard): formate the files

--- dashboard/src/pages/Logout/index.jsx
+++ dashboard/src/pages/Logout/index.jsx
@@ -1,39 +1,37 @@
-import React, { useEffect } from 'react';
-import axios from 'axios';
-import { useRouter } from 'next/router';
-const API_URL = "http://127.0.0.1:8000/api/dashboard/";
+import axios from "axios"
+import { useRouter } from "next/router"
+import { useEffect } from "react"
+const API_URL = "http://127.0.0.1:8000/api/dashboard/"
 const LogoutPage = () => {
-    const router = useRouter();
-    const logout = async () => {
-        try {
-            const headers = {
-                "Content-Type": `application/vnd.api+json`,
-                "Accept": `application/vnd.api+json`,
-                "Authorization": `Bearer ${localStorage.getItem('authToken')}`
-            };
-            const response = await axios({
-                method: 'post',
-                url: 'logout',
-                baseURL: API_URL,
-                headers: headers,
-            });
+  const router = useRouter()
+  const logout = async () => {
+    try {
+      const headers = {
+        "Content-Type": `application/vnd.api+json`,
+        Accept: `application/vnd.api+json`,
+        Authorization: `Bearer ${localStorage.getItem("authToken")}`,
+      }
+      const response = await axios({
+        method: "post",
+        url: "logout",
+        baseURL: API_URL,
+        headers: headers,
+      })
 
-            // مسح رمز المصادقة من Local Storage
-            localStorage.removeItem('authToken');
-            localStorage.removeItem('UserName');
-            console.log('تم تسجيل الخروج بنجاح!');
-            router.push('/Login');
-        } catch (error) {
-            console.error('حدث خطأ أثناء تسجيل الخروج:', error);
-        }
-    };
-    useEffect(() => {
+      // مسح رمز المصادقة من Local Storage
+      localStorage.removeItem("authToken")
+      localStorage.removeItem("UserName")
+      console.log("تم تسجيل الخروج بنجاح!")
+      router.push("/Login")
+    } catch (error) {
+      console.error("حدث خطأ أثناء تسجيل الخروج:", error)
+    }
+  }
+  useEffect(() => {
+    logout()
+  }, [])
 
+  return <div>جارٍ تسجيل الخروج...</div>
+}
 
-        logout();
-    }, []);
-
-    return <div>جارٍ تسجيل الخروج...</div>;
-};
-
-export default LogoutPage;
+export default LogoutPage

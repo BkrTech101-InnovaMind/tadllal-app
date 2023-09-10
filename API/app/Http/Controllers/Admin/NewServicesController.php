0d090a3807eb29cda511d6dd7799652f31cbc052
Ramiz Ali <ramiz.apk7@gmail.com>
2023-09-10 03:51:13 +0300

    new serices

--- API/app/Http/Controllers/Admin/NewServicesController.php
+++ API/app/Http/Controllers/Admin/NewServicesController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Http\Resources\NewServicesOrdersResource;
+use App\Models\NewServicesOrders;
 use Illuminate\Http\Request;
 use App\Traits\HttpResponses;
 use App\Http\Resources\NewServicesResource;
@@ -146,4 +148,70 @@ class NewServicesController extends Controller
 
         }
     }
+
+
+    public function getAllServicesOrders()
+    {
+        $orders = NewServicesOrders::with('user', 'NewServices')->get();
+
+        return $this->success([
+            'orders' => NewServicesOrdersResource::collection($orders),
+        ]);
+    }
+
+
+    public function approveServiceOrder($orderId)
+    {
+        $order = NewServicesOrders::find($orderId);
+
+        if (!$order) {
+            return $this->error('', 'Order not found', 404);
+        }
+
+        if ($order->status == 'Under Review') {
+            $order->status = 'Approved';
+        } else {
+            $order->status = 'Under Review';
+        }
+
+        $order->save();
+
+        return $this->success([
+            'message' => 'Order has been approved successfully',
+        ]);
+    }
+
+    public function getApprovedServiceOrders()
+    {
+        $approvedOrders = NewServicesOrders::where('status', 'Approved')->with('user', 'subConstructionService')->get();
+
+        return $this->success([
+            'approvedOrders' => NewServicesOrdersResource::collection($approvedOrders),
+        ]);
+    }
+
+    public function getPendingServiceOrders()
+    {
+        $pendingOrders = NewServicesOrders::where('status', 'Under Review')->with('user', 'subConstructionService')->get();
+
+        return $this->success([
+            'pendingOrders' => NewServicesOrdersResource::collection($pendingOrders),
+        ]);
+    }
+
+    public function destroyServiceOrder($orderId)
+    {
+        $order = NewServicesOrders::find($orderId);
+
+        if (!$order) {
+            return $this->error('', 'Order not found', 404);
+        }
+
+        $order->delete();
+
+        return $this->success([
+            'message' => 'Order has been deleted successfully',
+        ]);
+    }
+
 }
